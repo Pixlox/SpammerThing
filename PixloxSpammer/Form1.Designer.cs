de51050af46ea882543b222fd8b4ec96f6fc5ea8
Pixlox <85137818+Pixlox@users.noreply.github.com>
2021-07-04 17:12:29 +1000

    Initial Commit 2
    Intital commit, but the actual initial commit lol

---
+++ PixloxSpammer/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace PixloxSpammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.spamtextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerintervalbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // spamtextbox
+            // 
+            this.spamtextbox.Location = new System.Drawing.Point(49, 110);
+            this.spamtextbox.Name = "spamtextbox";
+            this.spamtextbox.Size = new System.Drawing.Size(221, 22);
+            this.spamtextbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Spam Text";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Timer Interval";
+            // 
+            // timerintervalbox
+            // 
+            this.timerintervalbox.Location = new System.Drawing.Point(49, 172);
+            this.timerintervalbox.Name = "timerintervalbox";
+            this.timerintervalbox.Size = new System.Drawing.Size(221, 22);
+            this.timerintervalbox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto Light", 18F);
+            this.label3.Location = new System.Drawing.Point(118, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 37);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Pixlox\'s Spammer";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-23, 242);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(528, 93);
+            this.panel1.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PixloxSpammer.Properties.Resources._2048px_Octicons_mark_github_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(467, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(491, 308);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timerintervalbox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.spamtextbox);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Pixlox\'s Spammer";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox spamtextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox timerintervalbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PixloxSpammer/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixloxSpammer
+{
+    public partial class Form1 : Form
+    {
+
+        int tInverval; // timer interval variable
+        int spamAmount; // spam amount variable
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // start button
+        {
+            tInverval = Convert.ToInt32(timerintervalbox.Text); // grab whatevers inside timer interval tb, then convert it into an integer, and put it into tInterval.
+            timer1.Interval = tInverval; // set the interval to whatevers inside tInverval, which is the text inside.
+            spamAmount = 0; // set spamAmount variable to 0.
+            timer1.Start(); // start the timer (spam)
+        }
+
+        private void button2_Click(object sender, EventArgs e) // stop button
+        {
+            timer1.Stop(); // stop the timer
+            timer1.Interval = 1; // reset timer interval to 1.  
+            tInverval = 1; // reset tInterval to 1.
+            MessageBox.Show("Spam stopped. Total messages sent is: " + spamAmount + "."); // say how much spam has been sent using spamAmount.
+            spamAmount = 0; // reset spamAmount before as a failsafe.
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // timer
+        {
+            SendKeys.Send(spamtextbox.Text); // send whatever is inside every 'tick'. which is every [insert tInterval here] ms.
+            SendKeys.Send("{Enter}"); // send Enter Key.
+            spamAmount = spamAmount + 1; // set spam amount to +1 whatever it was before.
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", "https://github.com");
+        }
+    }
+}
